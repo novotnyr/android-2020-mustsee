e5cc0bf937f475328d71745157affc5e608974c5
Robert Novotny <novotnr0@gmail.com>
2020-04-25 13:08:26 +0200

    Zavedeny RecyclerView do layoutu a kodu

--- app/src/main/java/com/github/novotnyr/android/mustsee/MainActivity.java
+++ app/src/main/java/com/github/novotnyr/android/mustsee/MainActivity.java
@@ -1,14 +1,19 @@
 package com.github.novotnyr.android.mustsee;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.*;
 
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
+    private RecyclerView placeRecyclerView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        placeRecyclerView = findViewById(R.id.placeRecyclerView);
+        placeRecyclerView.setLayoutManager(new LinearLayoutManager(this));
     }
 }
